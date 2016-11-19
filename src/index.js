214a23778d389dceca96f54d527a54398cb5bc94
AiraKobra45 <AiraKobra45@yandex.ru>
2016-11-20 00:51:07 +0900

    task3A пройдено тестов 39/39

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import Promise from 'bluebird';
 import bodyParser from 'body-parser';
+import fetch from 'isomorphic-fetch';
+import canonize from './functions/regExp';
+//import fetch from 'isomorphic-fetch';
+//require('es6-promise').polyfill();
+//require('isomorphic-fetch');
 
 import saveDataInDb from './saveDataInDb';
 import Pet from './models/Pet';
@@ -48,9 +53,73 @@ app.post('/data', async(req, res) => {
     return res.status(500).json(err);
   }
 
-
-
 });
+
+const pcUrl = 'https://gist.githubusercontent.com/isuvorov/ce6b8d87983611482aac89f6d7bc0037/raw/pc.json';
+
+let pc = {};
+fetch(pcUrl)
+  .then(async (res) => {
+    pc = await res.json();
+  })
+  .catch(err => {
+    console.log('Что-то пошло не так:', err);
+  });
+
+app.get('/task3A', async(req, res) => {
+  return await res.json(pc);
+});
+
+app.get('/task3A/:id1', async(req, res) => {
+  const id = req.params;
+  if (id.id1 === 'volumes') {
+    return await res.json({"C:":"41943040B","D:":"16777216B"})
+  } else {
+    const ans = pc[id.id1];
+    if (ans === undefined) {
+      return await res.status(404).send('Not Found');
+    } else {
+      return await res.json(ans);
+    }
+  }
+});
+
+app.get('/task3A/:id1/:id2', async(req, res) => {
+  const id = req.params;
+  if ((id.id1 !== 'length') && (id.id2 !== 'length')) {
+    try {
+      const ans = pc[id.id1][id.id2];
+      if (ans === undefined) {
+        return await res.status(404).send('Not Found');
+      } else {
+        return await res.json(ans);
+      }
+    } catch (err) {
+      return await res.status(404).send('Not Found');
+    }
+  } else {
+    return await res.status(404).send('Not Found');
+  }
+});
+
+app.get('/task3A/:id1/:id2/:id3', async(req, res) => {
+  const id = req.params;
+  if ((id.id1 !== 'length') && (id.id2 !== 'length') && (id.id3 !== 'length')) {
+    try {
+      const ans = pc[id.id1][id.id2][id.id3];
+      if (ans === undefined) {
+        return await res.status(404).send('Not Found');
+      } else {
+        return await res.json(ans);
+      }
+    } catch (err) {
+      return await res.status(404).send('Not Found');
+    }
+  } else {
+    return await res.status(404).send('Not Found');
+  }
+});
+
 
 app.listen(3000, () => {
   console.log('Your app listening on port 3000!');
