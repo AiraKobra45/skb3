65cda1e960ddf03849d04ce20e4b75e07416f4a1
AiraKobra45 <AiraKobra45@yandex.ru>
2016-11-26 05:47:25 +0900

    task3B пройдено тестов 31/61 	/users?havePet=cat

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import Promise from 'bluebird';
 import bodyParser from 'body-parser';
 import fetch from 'isomorphic-fetch';
+
 //import fetch from 'isomorphic-fetch';
 //require('es6-promise').polyfill();
 //require('isomorphic-fetch');
@@ -127,7 +128,136 @@ function volume() {
   }
   return result;
 }
+//
+/////////   TASK 3B    /////////   TASK 3B    /////////   TASK 3B    /////////   TASK 3B    /////////
+//
 
+const pcUrl2 = 'https://gist.githubusercontent.com/isuvorov/55f38b82ce263836dadc0503845db4da/raw/pets.json';
+
+let base = {};
+fetch(pcUrl2)
+  .then(async (res) => {
+    base = await res.json();
+  })
+  .catch(err => {
+    console.log('Что-то пошло не так: 2', err);
+  });
+
+app.get('/task3B', async(req, res) => {
+  return await res.json(base);
+
+});
+
+app.get('/task3B/users', async(req, res) => {
+  return await res.json(base.users);
+
+});
+/*
+app.get('/task3B/pets', async(req, res) => {
+  const type = req.query.type;
+  let len = base.pets.length;
+  let timeBase = [];
+  if (type === 'cat') {
+    for (var i = 0; i < len;  i++) {
+      if (base.pets[i].type === 'cat') {
+        timeBase.push(base.pets[i]);
+      }
+    }
+   return res.json((timeBase));
+  } else  return await res.send(base.pets);
+});*/
+
+app.get('/task3B/pets', async(req, res) => {
+  const type = req.query.type;
+  const age_gt = req.query.age_gt;
+  const age_lt = req.query.age_lt;
+  //console.log('* '+ type + '* '+ age_gt + '* '+ age_lt);
+  //console.log('Start');
+  if (type) {//console.log('1');
+    //return await res.json(getPets(base.pets, type, age_gt));
+    if (age_gt && !age_lt) { //console.log('2');
+      return await res.json(getPets(getPetsAge(base.pets, age_gt, true), type));
+    } else if (age_lt && !age_gt) {//console.log('3');
+      return await res.json(getPets(getPetsAge(base.pets, age_lt, false), type));
+    } else if (age_lt && age_gt) {//console.log('4');
+      return await res.json(getPets(getPetsAge(getPetsAge(base.pets,age_lt, false),age_gt,true), type));
+    } else //console.log('Type');
+      return await res.json(getPets(base.pets, type));
+  } else if (age_gt && !age_lt) { //console.log('5');
+    return await res.json(getPetsAge(base.pets, age_gt, true));
+  } else if (age_lt && !age_gt) {//console.log('6');
+    return await res.json(getPetsAge(base.pets, age_lt, false));
+  } else //console.log('7');
+    return await res.json(base.pets);
+  //console.log('The End');
+});
+
+function getPetsAge(base0, age1, bool) {
+  let base1 = base0;
+  let len = base1.length;
+  let timeBase = [];
+  let age = +age1;
+  for (var i = 0; i < len;  i++) {
+    switch (bool) {
+      case true:
+        if (base1[i].age > age) {
+          timeBase.push(base1[i]);
+          //console.log(base1[i].id + '_' + base1[i].age + '>' + age);
+          }
+        break;
+      case false:
+        if (base1[i].age < age) {
+          timeBase.push(base1[i]);
+          //console.log(base1[i].id + '_' +base1[i].age + '<' + age);
+        }
+        break;
+    }
+  }
+  return timeBase;
+}
+
+function getPets(base1, pet) {
+  let len = base1.length;
+  let timeBase = [];
+  for (var i = 0; i < len;  i++) {
+    if (base1[i].type == pet) {
+      console.log(base1[i]);
+      timeBase.push(base1[i]);
+    }
+  }
+  return timeBase;
+}
+/*
+function getPets(base1, pet, age1) {
+  let len = base1.length;
+  let timeBase = [];
+  let age = +age1;
+  for (var i = 0; i < len;  i++) {
+    if (base1[i].type == pet) {
+      if ((0 < age) && (age < base1[i].age) ) {
+        timeBase.push(base1[i]);
+      }
+    }
+  }
+  return timeBase;
+}
+*/
+app.get('/task3B/pets/:id1', async(req, res) => {
+  const id = req.params;
+  console.log(id);
+  if (base.pets[id.id1 - 1]) {
+    return await res.json(base['pets'][id.id1-1]);
+  } else  return await res.status(404).send('Not Found');
+});
+
+app.get('/task3B/users/:id1', async(req, res) => {
+  const id = req.params;
+  console.log(id);
+  if (base.users[id.id1 - 1]) {
+    return await res.json(base['users'][id.id1-1]);
+  } else  return await res.status(404).send('Not Found');
+
+});
 
 app.listen(3000, () => {
   console.log('Your app listening on port 3000!');
